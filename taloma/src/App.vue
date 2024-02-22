692be32a09349be3617e1f629a3ccb6c741f2af4
kahae0617 <kahae0617@gmail.com>
2024-02-22 14:01:00 +0800

    2/22 首頁layout

--- taloma/src/App.vue
+++ taloma/src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/logo.png" width="70" height="auto" class="align-text-top alt=">
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link disabled">Disabled</a>
+        </div>
+      </div>
+    </div>
   </nav>
   <router-view/>
 </template>
